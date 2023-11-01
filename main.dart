739a7b69a34717ad7bcd8c0688519ffc8c034bae
ArdaBerat41 <145961405+ArdaBerat41@users.noreply.github.com>
2023-11-01 16:27:06 +0300

    Bellingham

--- main.dart
+++ main.dart
@@ -15,65 +15,73 @@ class Uygicardi extends StatelessWidget {
         alignment: Alignment.center,
         width: double.infinity,
         color: Colors.yellow,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Kart"),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      topRight: Radius.circular(16))),
-              width: 300,
-              height: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text("Profil"), Icon(Icons.person)],
+        child: Container(
+          margin: EdgeInsets.only(left: 100, right: 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Kart"),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        topRight: Radius.circular(16))),
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("Profil"), Icon(Icons.person)],
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              color: Colors.white,
-              width: 300,
-              child: Column(
-                children: [
-                  CircleAvatar(radius:100,backgroundImage: AssetImage("images/bellingham.jpg"),),
-                  Text("Jude Bellingham"),
-                  Text("bellingham@madrid.com"),
-                ],
+              Container(
+                padding: EdgeInsets.all(8.0),
+                color: Colors.white,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 100,
+                      backgroundImage: AssetImage("images/bellingham.jpg"),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Jude Bellingham"),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("bellingham@madrid.com"),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              color: Colors.white,
-              width: 300,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Edit"),
-                  Icon(Icons.edit),
-                ],
+              Container(
+                padding: EdgeInsets.all(8.0),
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Edit"),
+                    Icon(Icons.edit),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(16),
-                  bottomRight: Radius.circular(16),
-                )
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(16),
+                      bottomRight: Radius.circular(16),
+                    )),
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("Durum"), Icon(Icons.assignment)],
+                ),
               ),
-              width: 300,
-              height: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text("Durum"), Icon(Icons.assignment)],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       )),
     );

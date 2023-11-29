e1263d636f74155c08ff76596f9dbc23c7cc1811
ArdaBerat41 <145961405+ArdaBerat41@users.noreply.github.com>
2023-11-29 15:14:26 +0300

    DartResimSyc

--- main.dart
+++ main.dart
@@ -1,92 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(snv());
+  runApp(Kronometre());
 }
 
-class snv extends StatelessWidget {
-  const snv({super.key});
+class Kronometre extends StatefulWidget {
+  const Kronometre({super.key});
 
+  @override
+  State<Kronometre> createState() => _KronometreState();
+}
+
+class _KronometreState extends State<Kronometre> {
+  int sayac =10;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Debit Cards"),
+          title: Text("Kronometre"),
         ),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.greenAccent,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 12),
-                child: Text(
-                  "Debit Cards",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-              ),
-
-              Image.asset("images/bankCard.jpg"),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 50),
-                    child: Column(
-                      children: [Icon(Icons.account_balance),Text("Send")],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 50),
-                        child: Column(
-                          children: [Icon(Icons.account_balance_wallet),Text("Top Up")],
-                        ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 50),
-                        child: Column(
-                          children: [Icon(Icons.add_card),Text("Hecive")],
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-              Container(
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.white),
-                width: 150,
-                height: 25,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 50),
-                      child: Column(
-                        children:[Icon(Icons.add_box_outlined),Text("")],
-                      ),
-                      
-
-
-                    )
-                  ],
-                ),
-              ),
-            ],
+        body: Center(
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                sayac--;
+                if(sayac==0){
+                  sayac=0;
+                }
+              });
+            },
+            child: Container(
+              child: Image.asset("images/number-$sayac.png"),
+            ),
           ),
         ),
       ),
     );
   }
+
+  degistir() {
+    sayac--;
+    print(sayac);
+  }
 }
+
